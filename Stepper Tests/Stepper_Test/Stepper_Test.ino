4eea89c9a50bbd1577601a3cef4d8b8ab55ec717
willtmakinen78 <35124538+willtmakinen78@users.noreply.github.com>
2018-04-17 21:30:58 -0400

    Doing some cleanup

---
+++ Stepper Tests/Stepper_Test/Stepper_Test.ino
@@ -0,0 +1,18 @@
+const int stepPin = 2;
+const int dirPin = 3;
+
+void setup() {
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+
+  digitalWrite(dirPin, HIGH);
+
+}
+
+void loop() {
+  digitalWrite(stepPin, HIGH);
+  delayMicroseconds(2.5);
+  digitalWrite(stepPin, LOW);
+  delayMicroseconds(3000);
+
+}
