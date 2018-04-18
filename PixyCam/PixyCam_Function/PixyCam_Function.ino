891264d9a2af70f901615e015cfc5fcfcc7ef630
willtmakinen78 <35124538+willtmakinen78@users.noreply.github.com>
2018-04-17 21:33:11 -0400

    some starting points for the PixyCam

---
+++ PixyCam/PixyCam_Function/PixyCam_Function.ino
@@ -0,0 +1,53 @@
+#include <SPI.h>
+#include <Pixy.h>
+
+Pixy pixy;
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.print("Starting...\n");
+
+  pixy.init();
+}
+
+void loop() {
+  float tarX = targetPosition(4);
+  static float lastTarX;
+
+  if (tarX != lastTarX) {
+    //turn towards target
+    lastTarX = tarX;
+  }
+  Serial.print("Target X: "); Serial.println(tarX);
+}
+float targetPosition(int refreshPerSec) {
+  int refreshInterval = 50 / refreshPerSec;
+  static int counter = 0;
+  uint16_t blocks;
+  char buf[32];
+  static float avX;
+
+  blocks = pixy.getBlocks();
+
+  if (blocks) {
+    counter++;
+    if (counter % refreshInterval == 0) {
+      avX = 0;
+      //            sprintf(buf, "Detected %d:\n", blocks);
+      //            Serial.print(buf);
+
+      for (int j = 0; j < blocks; j++) {
+        //                sprintf(buf, "  block %d: ", j);
+        //                Serial.print(buf);
+        //                pixy.blocks[j].print();
+
+        avX += pixy.blocks[j].x;
+      }
+      avX /= blocks;
+      //       Serial.print("Average X: "); Serial.println(avX);
+    }
+  }
+  return avX;
+}
+
---
+++ PixyCam/PixyCam_Test/PixyCam_Test.ino
@@ -0,0 +1,43 @@
+#include <SPI.h>  
+#include <Pixy.h>
+
+Pixy pixy;
+
+int refreshPerSec = 4;
+int refreshInterval;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.print("Starting...\n");
+
+  pixy.init();
+
+  refreshInterval = 50/refreshPerSec;
+}
+
+void loop() {
+  static int counter = 0;
+  uint16_t blocks;
+  char buf[32]; 
+  blocks = pixy.getBlocks();
+
+  if (blocks) {
+    counter++;
+    if (counter % refreshInterval == 0) {
+      sprintf(buf, "Detected %d:\n", blocks);
+      Serial.print(buf);
+
+      float avX;
+      for (int j = 0; j < blocks; j++) {
+        sprintf(buf, "  block %d: ", j);
+        Serial.print(buf); 
+        pixy.blocks[j].print();
+
+        avX += pixy.blocks[j].x;
+      }
+      avX /= blocks;
+      Serial.print("Average X: "); Serial.println(avX);
+    }
+  }
+
+}
